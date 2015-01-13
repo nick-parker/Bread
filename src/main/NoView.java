7b031ba347011f134cd71a7d85180071ba1868d4
nick-parker <github@nicholasparker.org>
2015-01-13 15:44:43 -0800

    Disabled minZ checking
    minZ checking was introducing discontinuities which I don't want to deal
    with while implementing shells, so i disabled it and added a TODO note.

--- src/main/NoView.java
+++ src/main/NoView.java
@@ -17,15 +17,15 @@ public class NoView {
 		Surface3D m2 = null;		
 		Slicer s = null;
 		try {
-			m1 = Stli.importModel("dragon_lowpoly.stl", true);
+			m1 = Stli.importModel("vtop.stl", true);
 //			m1.move(new Vector3D(0,0,0.25));
-			m2 = Stli.importSurface("dragonv.stl", true);
+			m2 = Stli.importSurface("v.stl", true);
 //			m2 = SimplePlane.MakePlane(-200, -200, 200, 200, 4);
 //			m2.move(new Vector3D(2.5,0,0));
 			//(Model3D part, Surface3D shape, layerHeight, filD, nozzleD, 
 			//extrusionWidth, PrintTemp, xySpeed, zSpeed, numShells, infillWidth, 
 			//infillDir, infillAngle, lift, retraction)
-			s = new Slicer(m1, m2, "config_ccecil.txt");
+			s = new Slicer(m1, m2, "config1.txt");
 //			s = new Slicer(m1, m2, 0.3, 1.75, 0.4, .55, 200, 40, 40, 0, 0.6, 0, Math.PI/4, 0.15, 0, 50);
 		} catch (FileNotFoundException e){
 			System.out.println(e.getMessage());
@@ -35,6 +35,6 @@ public class NoView {
 		}
 		String test = "0 0 200 0 200 200 0 200 :";
 		System.out.println(ClipperJNA.inset(test, -5));
-//		s.slice("ccecil.gcode");
+		s.slice("shells.gcode");
 	}
 }
