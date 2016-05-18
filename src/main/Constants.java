a60bcf767a28f2868568864d536233107a2750e0
Nick Parker <nmp67@cornell.edu>
2016-05-18 15:23:07 -0400

    Fix localization issue in GcodeExport
    Fixes Github Issue #7 where certain locales use , instead of .
    for decimals in the DecimalFormat. Locale for formatting now
    set to en-us.

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 import math.geom2d.Point2D;
 import math.geom3d.Point3D;
@@ -18,7 +20,8 @@ public class Constants {
 	static public final Vector3D zminus = new Vector3D(0,0,-1);
 	static public final double tol = 1e-4;
 	static public final double solidSpacing = 1.09;
-	static public DecimalFormat ext = new DecimalFormat("#.#####");	//Extrusion
-	static public DecimalFormat xyz = new DecimalFormat("#.###");		//Position
+	static private java.text.DecimalFormatSymbols enus = new DecimalFormatSymbols(Locale.ENGLISH);
+	static public DecimalFormat ext = new DecimalFormat("#.#####", enus);	//Extrusion
+	static public DecimalFormat xyz = new DecimalFormat("#.###", enus);		//Position
 	static public Vector3D zero =  new Vector3D(0,0,0);
 }
